a46df4c7090a536bd06b3b33ec9d0c278783de17
Patryk Palej <patrykpalej@gmail.com>
2024-09-08 15:05:07 +0200

    Kółko i krzyżyk 2

--- KiK/main.py
+++ KiK/main.py
@@ -3,3 +3,20 @@ import numpy as np
 
 def generate_board():
     return np.full((3, 3), ".")
+
+
+def put_item_on_board(board, coordinates, mark):
+    if mark not in ["x", "o"]:
+        return board, False
+
+    if coordinates[0] < 0 or coordinates[0] > 2 or coordinates[1] < 0 or coordinates[1] > 2:
+        return board, False
+
+    if board[coordinates] != ".":
+        return board, False
+
+    # ... itd.
+
+    board[coordinates] = mark
+
+    return board, True
